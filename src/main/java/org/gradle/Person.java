a371953128286ed69a6c8147976d0bc31b57d652
hmetaute <hernan.metaute@gmail.com>
2013-08-21 14:58:06 -0500

    se arregla para que compile

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -2,15 +2,18 @@ package org.gradle;
 
 import org.apache.commons.collections.list.GrowthList;
 
+
+
 public class Person {
     private final String name;
+    
+
 
     public Person(String name) {
-        this.name = name;
-        new GrowthList();
+        this.name = name;        
     }
 
     public String getName() {
-    	return "valor quemado";
+    	return this.name;
     }
 }
